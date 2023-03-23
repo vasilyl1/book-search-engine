e5ea8fea9c803a7fc75bb2e6d23f9c593e4d9b6c
vasilyl1 <likhovaido@gmail.com>
2023-03-23 19:00:58 -0400

    models and initial for resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,35 @@
+const { Tech, Matchup } = require('../models');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+        return User.find().populate('thoughts');
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username }).populate('thoughts');
+      },
+    tech: async () => {
+      return Tech.find({});
+    },
+    matchups: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Matchup.find(params);
+    },
+  },
+  Mutation: {
+    createMatchup: async (parent, args) => {
+      const matchup = await Matchup.create(args);
+      return matchup;
+    },
+    createVote: async (parent, { _id, techNum }) => {
+      const vote = await Matchup.findOneAndUpdate(
+        { _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return vote;
+    },
+  },
+};
+
+module.exports = resolvers;
