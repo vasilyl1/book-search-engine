99d9500350e1bbe183275cd97289c2174d12fbcb
vasilyl1 <likhovaido@gmail.com>
2023-03-26 16:58:17 -0400

    mutations correction for the login process

--- client/src/components/utils/mutations.js
+++ client/src/components/utils/mutations.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
+        token
         user {
             email
             username
@@ -15,6 +16,7 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
+        token
       user {
         username
         email
