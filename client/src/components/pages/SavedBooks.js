fc3e48c735f9f9d5c8e18c138edb2ee5441d1427
vasilyl1 <likhovaido@gmail.com>
2023-03-27 18:12:24 -0400

    logout reload

--- client/src/components/pages/SavedBooks.js
+++ client/src/components/pages/SavedBooks.js
@@ -15,18 +15,15 @@ import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-    const [userData, setUserData] = useState({}); // userData state
+    
     const [savedBooksData, setBooksData] = useState([]);
-
     const [deleteBook, { error }] = useMutation(DELETE_BOOK); // use to delete book
-
     const { loading, data } = useQuery(QUERY_USER, { variables: { username: Auth.getProfile().data.username } }); // query user to get saved books
-
     useEffect(() => {
         const getSavedBooksData = async () => {
             try {
-                    const savedBooks = data.user?.savedBooks || []; // get the savedbooks array data from usequery
-                    setBooksData(savedBooks); // update the state
+                const savedBooks = data.user?.savedBooks || []; // get the savedbooks array data from usequery
+                setBooksData(savedBooks); // update the state
             } catch (err) {
                 console.log(err);
             }
@@ -50,52 +47,53 @@ const SavedBooks = () => {
             console.error(err);
         }
     };
-
-    // if data isn't here yet, say so
-    //if (loading) return <h2>LOADING...</h2>;
-    if (!Auth.loggedIn)
-        return (
-            <h4>
-                You need to be logged in to see this. Use the navigation links above to
-                sign up or log in!
-            </h4>
-        );
-
+    
     return (
         <>
-            <div fluid className="text-light bg-dark p-5">
-                <Container>
-                    <h1>Viewing saved books!</h1>
-                </Container>
-            </div>
-            <Container>
-                <h2 className='pt-5'>
-                    {savedBooksData.length
-                        ? `Viewing ${savedBooksData.length} saved ${savedBooksData.length === 1 ? 'book' : 'books'}:`
-                        : 'You have no saved books!'}
-                </h2>
-                <Row>
-                    {savedBooksData.map((book) => {
-                        return (
-                            <Col md="4">
-                                <Card key={book.bookId} border='dark'>
-                                    {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                                    <Card.Body>
-                                        <Card.Title>{book.title}</Card.Title>
-                                        <p className='small'>Authors: {book.authors}</p>
-                                        <Card.Text>{book.description}</Card.Text>
-                                        <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                                            Delete this Book!
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        );
-                    })}
-                </Row>
-            </Container>
+            {Auth.loggedIn() ? (
+                <>
+                    <div fluid className="text-light bg-dark p-5">
+                        <Container>
+                            <h1>Viewing saved books!</h1>
+                        </Container>
+                    </div>
+                    <Container>
+                        <h2 className='pt-5'>
+                            {savedBooksData.length
+                                ? `Viewing ${savedBooksData.length} saved ${savedBooksData.length === 1 ? 'book' : 'books'}:`
+                                : 'You have no saved books!'}
+                        </h2>
+                        <Row>
+                            {savedBooksData.map((book) => {
+                                return (
+                                    <Col md="4">
+                                        <Card key={book.bookId} border='dark'>
+                                            {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                                            <Card.Body>
+                                                <Card.Title>{book.title}</Card.Title>
+                                                <p className='small'>Authors: {book.authors}</p>
+                                                <Card.Text>{book.description}</Card.Text>
+                                                <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                                                    Delete this Book!
+                                                </Button>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                );
+                            })}
+                        </Row>
+                    </Container>
+                </>
+            ) : (
+                <h4>
+                    You need to be logged in to see this. Use the navigation links above to
+                    sign up or log in!
+                </h4>
+            )}
         </>
+
     );
+
 };
 
 export default SavedBooks;
