a3c561a638ebf5e43991dcf340350d5eac813ada
vasilyl1 <likhovaido@gmail.com>
2023-03-28 08:42:54 -0400

    profile link change

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -32,7 +32,7 @@ const AppNavbar = () => {
                             {/* if user is logged in show saved books and logout option */}
                             {Auth.loggedIn() ? (
                                 <>
-                                    <Link className="btn btn-lg btn-info m-2" to="/me">
+                                    <Link className="btn btn-lg btn-info m-2" to="/saved">
                                         {Auth.getProfile().data.username}'s profile
                                     </Link>
                                     <button className="btn btn-lg btn-light m-2" onClick={logout}>
